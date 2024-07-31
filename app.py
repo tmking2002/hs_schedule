231cc9b66b62e43f5d2247698ade6c5091835529
tmking2002 <tmking4@ncsu.edu>
2024-07-30 21:35:17 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import streamlit as st
+
+schedule = pd.read_csv('schedule.csv')
+
+schedule = schedule[schedule['Date'] != 'Date TBA']
+
+schedule["Opponent"] = schedule["Opponent"].str.replace("@", "").str.replace("vs", "").str.replace("*", "").str.strip()
+schedule["Date"] = pd.to_datetime(schedule["Date"] + "/2024", format='%m/%d/%Y').dt.date
+
+st.title('2024 Fall Schedule')
+st.subheader('Georgia HS')
+
+default_teams = ['Riverwood Raiders', 'Brookwood Broncos', 'Pierce County Bears', 'Mount Paran Christian Eagles', 
+                 'Buford Wolves', 'East Coweta Indians', 'Cherokee Bluff Bears', 'East Forsyth Broncos', 
+                 'West Forsyth Wolverines', 'Northside Eagles']
+
+all_teams = schedule['Team'].unique().sort()
+
+teams = st.multiselect('Select Teams', all_teams, default=default_teams)
+
+filtered_schedule = schedule[schedule['Team'].isin(teams)]
+
+start_date = st.date_input('Start Date', pd.to_datetime('2024-08-01'))
+end_date = st.date_input('End Date', pd.to_datetime('2024-12-31'))
+
+filtered_schedule = filtered_schedule[(filtered_schedule['Date'] >= start_date) & (filtered_schedule['Date'] <= end_date)]
+
+filtered_schedule = filtered_schedule[['Date', 'Time', 'Team', 'Opponent']].sort_values(by=['Date', 'Time'])
+
+st.dataframe(filtered_schedule, use_container_width=True)
